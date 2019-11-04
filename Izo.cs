23f3f072f78c57b36ac9e874895740e6fa943b67
K0O7 <57181698+K0O7@users.noreply.github.com>
2019-11-04 18:03:36 +0100

    skrypty na poruszanie sie na planszy point and click i locked camera

---
+++ Izo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Izometryczny : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        
+            transform.position = player.position  ;
+
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    public LayerMask whatCanBeClickedOn;
+
+    private UnityEngine.AI.NavMeshAgent myAgent;
+
+    void Start()
+    {
+        myAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(myRay, out hitInfo, 100, whatCanBeClickedOn))
+            {
+                myAgent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
