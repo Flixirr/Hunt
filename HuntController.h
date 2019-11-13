2de1a6a2b543acc0435946d21e487e7fad754a61
Sewerus <57177519+Sewerus@users.noreply.github.com>
2019-11-13 13:17:43 +0100

    Motion Control
    Klasa gracz plus moduly sterujace ruchem.

---
+++ HuntController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "HuntController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOTIONCONTROL_API AHuntController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
